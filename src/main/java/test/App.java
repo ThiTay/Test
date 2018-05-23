2c392e6ab89115468345f728ec7469454978c36a
thib <thibault.tayebi@ctie.etat.lu>
2018-05-23 10:40:41 +0200

    q

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World the code has changed for the fourth time   !" );
+        System.out.println( "Hello World the code has changed for the fourth time      !" );
     }
 }
